e50380dae5ef14c96010447d7e0d2aa97572d25a
vridhi <rajeevvridhi@gmail.com>
2026-07-22 11:42:59 +0530

    feat: add Navbar component and new routes for image generation and resume review

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import Layout from './pages/Layout.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import WriteArticle from './pages/WriteArticle.jsx'
 import BlogTitles from './pages/BlogTitles.jsx'
+import GenerateImages from './pages/GenerateImage.jsx'
+import RemoveBackground from './pages/RemoveBackground.jsx'
+import RemoveObject from './pages/RemoveObject.jsx'
+import ReviewResume from './pages/ReviewResume.jsx'
+import Community from './pages/Community.jsx'
 
 function App() {
     return(
@@ -15,6 +20,11 @@ function App() {
                 <Route index element ={<Dashboard/>}/>
                 <Route path='write-artile' element ={<WriteArticle/>}/>
                 <Route path='blog-titles' element ={<BlogTitles/>}/>
+                <Route path='generate-images' element ={<GenerateImages/>}/>
+                <Route path='remove-background' element ={<RemoveBackground/>}/>
+                <Route path='remove-object' element ={<RemoveObject/>}/>
+                <Route path='review-resume' element ={<ReviewResume/>}/>
+                <Route path='community' element ={<Community/>}/>
                 </Route> 
             </Routes>
         </div>
