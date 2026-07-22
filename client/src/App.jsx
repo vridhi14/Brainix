aa6cdde5f121e244707e7d395f9a7431b597b04d
vridhi <rajeevvridhi@gmail.com>
2026-07-22 11:24:57 +0530

    feat: implement routing and layout structure with new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
-
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Layout from './pages/Layout.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import WriteArticle from './pages/WriteArticle.jsx'
+import BlogTitles from './pages/BlogTitles.jsx'
 
 function App() {
+    return(
+        <div>
+            <Routes>
+                <Route path='/' element ={<Home/>}/>
+                <Route path='/ai' element ={<Layout/>}>
+                <Route index element ={<Dashboard/>}/>
+                <Route path='write-artile' element ={<WriteArticle/>}/>
+                <Route path='blog-titles' element ={<BlogTitles/>}/>
+                </Route> 
+            </Routes>
+        </div>
+    )
 }
 
 export default App
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react' ; 
+import { Outlet } from 'react-router-dom';
 
 function Layout(){
     return(
         <div>
             <h1>Layout</h1>
+            <Outlet/>
         </div>
     )
 }
