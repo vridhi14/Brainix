ce084b9c187b0c2c7bb02400eb8890291a8e54fa
vridhi <rajeevvridhi@gmail.com>
2026-07-24 19:48:53 +0530

    fix: correct syntax errors and improve layout in AiTools and Hero components

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import { assets } from '../assets/assets';
 function Hero(){
 
   const navigate = useNavigate(); 
 
   return (
-    <div className='px-4 sm:px-20 xl:px-32 relative inline-flex felx-col 
-    w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
+    <div className='px-4 sm:px-20 xl:px-32 relative flex flex-col items-center justify-center min-h-screen pt-28 bg-[url("/gradientBackground.png")] bg-cover bg-center bg-no-repeat'>
       
-      <div className='text-center mb-6'>
-        <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2'>Create Amazing Content with <br/> <span className='text-primary'>AI tools</span></h1>
-        <p>Transform your content creation with our suite of premium AI tools. 
-          Write articles , generate images , and enhance your workflow </p>
+      <div className='text-center max-w-4xl mx-auto'>
+        <h1 className='text-4xl sm:text-6xl lg:text-7xl font-semibold leading-tight tracking-tight'> Create amazing content <br /> with <span className='text-primary'>AI tools</span> </h1>
+        <p className='mt-6 max-w-xl mx-auto text-gray-600'> Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow. </p>
       </div>
 
-      <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs '>
+      <div className='flex justify-center gap-4 mt-8 text-sm max-sm:text-xs'>
         <button onClick ={()=>navigate('/ai')} className='bg-primary text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer'>Start creating now</button>
-        <button className='bg-white px-10 py-3 rounded-lg border boder-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</button>
+        <button className='bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</button>
       </div>
 
-      <div>
-        <img src={assestes.user_group} alt="" srcset="" className='h-8 '/>Trusted by 10k+ people
+      <div className='flex items-center gap-3 mt-8'>
+        <img src={assets.user_group} className='h-8' />
+        <p>Trusted by 10k+ people</p>
       </div>
 
     </div>
