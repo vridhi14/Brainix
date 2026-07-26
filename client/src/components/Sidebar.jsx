a48686801f89ddf2bb34bfcf48e93d69a958b06c
vridhi <rajeevvridhi@gmail.com>
2026-07-26 23:22:29 +0530

    feat: add navigation items to Sidebar component for improved user experience

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 import {useUser , useClerk} from '@clerk/clerk-react'
+import { Eraser, FileText, Hash, House, Image, Scissors, SquarePen, Users } from 'lucide-react';
+import { NavLink } from 'react-router-dom';
+
+const navItems = [
+    {to:'/ai' , label:'Dashboard' , Icon: House } , 
+    {to:'/ai/write-article' , label:'Write Article' , Icon: SquarePen } , 
+    {to:'/ai/blog-titles' , label:'Blog Titles' , Icon: Hash } , 
+    {to:'/ai/generate-images' , label:'Generate Images' , Icon: Image } , 
+    {to:'/ai/remove-background' , label:'Remove Background' , Icon: Eraser } , 
+    {to:'/ai/remove-object' , label:'Remove Object' , Icon: Scissors } , 
+    {to:'/ai/review-resume' , label:'Review Resume' , Icon: FileText } , 
+    {to:'/ai/community' , label:'Community' , Icon: Users } , 
+]
 
 function Sidebar({sidebar , setSidebar}){
 
@@ -13,6 +26,16 @@ function Sidebar({sidebar , setSidebar}){
         <div className='my-7 w-full'>
             <img src={user.imageUrl} alt="User avatar" className='w-13 rounded-full mx-auto'/>
             <h1 className='mt-1 text-center'>{user.fullName}</h1>
+
+            <div>
+                {navItems.map(({to,label , Icon}) =>(
+                    <NavLink key={to} to={to} end={to === '/ai'} onClick={()=>
+                        setSidebar(false)} className={({isActive}) =>`px-3.5 py-2.5 flex items-center gap-3 rounded ${isActive ? 'bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white' : ''}`}>
+                            {}
+
+                    </NavLink>
+                ))}
+            </div>
         </div>      
     </div>
   )
