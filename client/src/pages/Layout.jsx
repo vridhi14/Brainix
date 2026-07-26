2dcee21884f94c3c40a84a68e618f79428eb3fcd
vridhi <rajeevvridhi@gmail.com>
2026-07-26 22:41:35 +0530

    feat: enhance Layout component with sidebar navigation and logo click functionality

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,19 @@
-import React from 'react' ; 
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react' ; 
+import { Outlet, useNavigate, useSearchParams } from 'react-router-dom';
+import {assets } from '../assets/assets.js'
+import { Menu, X } from 'lucide-react';
 
 function Layout(){
+    const navigate = useNavigate();
+    const [sidebar , setSidebar] = useState(false); 
     return(
-        <div>
-            <h1>Layout</h1>
+        <div className='flex flex-col items-start justify-start h-screen'>
+            <nav>
+                <img src ={assets.logo} alt="" onClick={()=>navigate('/')}/>
+                {
+                    sidebar ? <X className='w-6 h-6 text-gray-600 sm:hidden'/> : <Menu className='w-6 h-6 text-gray-600 sm:hidden'/> 
+                }
+            </nav>
             <Outlet/>
         </div>
     )
