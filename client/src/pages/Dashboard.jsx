3b14063e4e7f4e49bf1dde06ecb76a170c47c941
vridhi <rajeevvridhi@gmail.com>
2026-07-30 22:42:56 +0530

    feat: implement dashboard data fetching and display total creations

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,33 @@
-import React from 'react' ; 
-
+import React, { useEffect, useState } from 'react' ; 
+import {dummyCreationData} from '../assets/assets.js'
+import { Sparkles } from 'lucide-react';
 function Dashboard(){
+
+    const [creations , setCreations] =useState([]); 
+    const getDashboardData = async()=>{
+        setCreations(dummyCreationData); 
+    }
+
+    useEffect(()=>{
+        getDashboardData()
+    },[])
+
+
     return(
-        <div>
-            <h1>Dashboard</h1>
+        <div className='h-full overflow-y-scroll p-6'>
+            <div className='flex justify-start gap-4 flex-wrap'>
+                <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounder-xl border border-bs-gray-200'>
+                    <div className='text-slate-600'>
+                        <p className='text-sm'>Total Creations</p>
+                        <h2 className='font-semibold text-xl'>{creations.length}</h2>
+                        </div>
+
+                    <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
+                        <Sparkles className='w-5 text-white'/>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -13,7 +13,7 @@ function Layout(){
     return user ? (
         <div className='flex flex-col items-start justify-start h-screen'>
             <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200'>
-                <img src ={assets.logo} alt="" onClick={()=>navigate('/')}/>
+                <img className='cursor-pointer w-32 sm:w-44'src ={assets.logo} alt="" onClick={()=>navigate('/')}/>
                 {
                     sidebar ? <X onClick ={()=> setSidebar(false)}className='w-6 h-6 text-gray-600 sm:hidden'/> : <Menu onClick ={()=> setSidebar(true)}className='w-6 h-6 text-gray-600 sm:hidden'/> 
                 }
