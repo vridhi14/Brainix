5faee4c5c96859cca47756c48ff36c0a1c10d088
vridhi <rajeevvridhi@gmail.com>
2026-08-02 20:28:46 +0530

    feat: add article length selection feature in WriteArticle component

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,8 +1,15 @@
 import { Sparkles } from 'lucide-react';
-import React from 'react' ; 
+import React, { useState } from 'react' ; 
 
 function WriteArticle(){
+    const articleLength = [
+        {length:800 , text : 'short(500-800words)'} ,
+        {length:1200 , text : 'medium(800-1200words)'} ,
+        {length:1600 , text : 'long(1200+ words)'} ,
+    ]
 
+    const [selectedLength , setSelectedLength] = useState(articleLength[0]);
+    const [input , setInput] = useState("");
     return(
         <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
             <form  className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
@@ -15,7 +22,15 @@ function WriteArticle(){
                 <input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='write your topic here...' required/>
 
                 <p className='mt-4 text-sm font-medium'>Article Length</p>
-                
+
+                <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+                    {articleLength.map((item , index)=>{
+                        <span key={index} 
+                        className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedLength.text=== item.text ? 'bg-blue-50 text-blue-700' : 'text-gray-500 border-gray-300' }`} 
+                        onClick={()=>{setSelectedLength(item)}}>{item.text}</span>
+                    })}
+                </div>
+
             </form>
 
             <div></div>
