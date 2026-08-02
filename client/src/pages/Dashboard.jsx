6fcec9a98927ed7cbafb9059958cb3cb22504fff
vridhi <rajeevvridhi@gmail.com>
2026-08-02 19:56:16 +0530

    feat: add CreationItem component to display individual creations in Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react' ; 
 import {dummyCreationData} from '../assets/assets.js'
-import { Sparkles } from 'lucide-react';
+import { Gem, Sparkles } from 'lucide-react';
 import {Protect} from '@clerk/clerk-react'
+import CreationItem from '../components/CreationItem.jsx';
+
+
 function Dashboard(){
 
     const [creations , setCreations] =useState([]); 
@@ -37,11 +40,18 @@ function Dashboard(){
                         </h2>
                         </div>
 
-                    <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
-                        <Sparkles className='w-5 text-white'/>
+                    <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
+                        <Gem className='w-5 text-white'/>
                     </div>
                 </div>
                 
+            </div>
+
+            <div className='space-y-3'> 
+                <p className='mt-6 mb-4'>Recent Creations</p>
+                {creations.map((item)=>{
+                    <CreationItem key={item.id} item={item}/>
+                })}
             </div>
             
         </div>
