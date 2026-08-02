d6a84c13a746a138116dcbf523b8f64321c68348
vridhi <rajeevvridhi@gmail.com>
2026-08-02 20:52:51 +0530

    feat: enhance BlogTitles and WriteArticle components with form handling and layout improvements

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,4 +1,4 @@
-import { Sparkles } from 'lucide-react';
+import { Edit, Sparkles } from 'lucide-react';
 import React, { useState } from 'react' ; 
 
 function WriteArticle(){
@@ -10,16 +10,21 @@ function WriteArticle(){
 
     const [selectedLength , setSelectedLength] = useState(articleLength[0]);
     const [input , setInput] = useState("");
+    
+    const onSubmitHandler = async(e)=>{
+        e.preventDefault();
+    }
     return(
         <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
-            <form  className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+            <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
                 <div className='flex items-center gap-3'>
                     <Sparkles className='w-6 text-[#4A7AFF]'/>
                     <h1 className='text-xl font-semibold'>Artile Configuration</h1>
                 </div>
                 <p className='mt-6 text-sm font-medium'>Article Topic</p>
 
-                <input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='write your topic here...' required/>
+                <input onChange={(e)=>{setInput(e.target.value)}} value={input}
+                type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='write your topic here...' required/>
 
                 <p className='mt-4 text-sm font-medium'>Article Length</p>
 
@@ -30,10 +35,26 @@ function WriteArticle(){
                         onClick={()=>{setSelectedLength(item)}}>{item.text}</span>
                     })}
                 </div>
-
+                <br />
+                <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg curso-pointer'>
+                    <Edit className='w-5'/>
+                    Generate Article
+                </button>
             </form>
 
-            <div></div>
+            <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+                <div className='flex items-center gap-3'>
+                    <Edit className='w-5 h-5 text-[#4A7AFF]'/>
+                    <h1 className='text-xl font-semibold'> Article Configuration</h1>
+                </div>
+
+                <div className='flex-1 flex justify-center items-center'>
+                    <div className='text-sm flex flex-col items-ceter gap-5 text-gray-400'>
+                    <Edit className='w-9 h-9'/>
+                    <p>Enter a topic nd click "Generate Article" to get started</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
