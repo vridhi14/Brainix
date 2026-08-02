912145019ed1b93dd4204db2f32e4b9d9355bd7c
vridhi <rajeevvridhi@gmail.com>
2026-08-02 20:15:56 +0530

    feat: enhance WriteArticle component layout and structure

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,9 +1,24 @@
+import { Sparkles } from 'lucide-react';
 import React from 'react' ; 
 
 function WriteArticle(){
+
     return(
-        <div>
-            <h1>WriteArticle</h1>
+        <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+            <form  className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+                <div className='flex items-center gap-3'>
+                    <Sparkles className='w-6 text-[#4A7AFF]'/>
+                    <h1 className='text-xl font-semibold'>Artile Configuration</h1>
+                </div>
+                <p className='mt-6 text-sm font-medium'>Article Topic</p>
+
+                <input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='write your topic here...' required/>
+
+                <p className='mt-4 text-sm font-medium'>Article Length</p>
+                
+            </form>
+
+            <div></div>
         </div>
     )
 }
