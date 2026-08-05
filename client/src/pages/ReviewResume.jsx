b4a3460a2b1c89227f19162b2439c7d8952536b3
vridhi <rajeevvridhi@gmail.com>
2026-08-05 23:06:04 +0530

    feat: implement community creations display with user interaction and data fetching

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -28,7 +28,7 @@ function RemoveResume(){
                 </button>
             </form>
 
-            <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 '>
+            <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
                 <div className='flex items-center gap-3'>
                     <FileText className='w-5 h-5 text-[#00DA83]'/>
                     <h1 className='text-xl font-semibold'> Analysis Results</h1>
