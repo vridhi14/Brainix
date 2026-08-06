8d9ac805e25df02269ddfdbaa2091b75938ddcfb
vridhi <rajeevvridhi@gmail.com>
2026-08-06 23:57:23 +0530

    feat: initialize server with Express and CORS setup in server.js

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors'; 
+import 'dotenv/config';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/', (req,res)=>{
+    res.send("server is live"); 
+})
