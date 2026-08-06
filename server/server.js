614cb126a0cbda7839499a9bfdfaa8017023ca76
vridhi <rajeevvridhi@gmail.com>
2026-08-06 23:58:52 +0530

    feat: add .env file for environment variable configuration and update server.js to use PORT from .env

--- server/server.js
+++ server/server.js
@@ -10,3 +10,5 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.send("server is live"); 
 })
+const PORT = process.env.PORT || 3000 ; 
+app.listen(PORT , ()=>console.log("server is running on port :",PORT)); 
