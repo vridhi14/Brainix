d0899fff966f86efc680ed3ff0e6a9754ea74518
vridhi <rajeevvridhi@gmail.com>
2026-08-07 23:04:39 +0530

    feat: add Clerk integration and update environment variables in .env and .gitignore

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors'; 
 import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express'
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); 
+app.use(clerkMiddleware()); 
 
 app.get('/', (req,res)=>{
     res.send("server is live"); 
